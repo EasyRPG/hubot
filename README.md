91613fb51cd936c3e914ac66a714dafd8a5e7798
take_cheeze <takechi101010@gmail.com>
2013-03-11 17:47:57 +0900

    add little description

---
+++ README.md
@@ -0,0 +1,7 @@
+personal hubot setting
+
+set the following env var before running run.sh
+* HUBOT_PORT
+* HUBOT_DOMAIN
+* HUBOT_IRC_NICK
+* HUBOT_GITHUB_TOKEN
--- run.sh
+++ run.sh
@@ -3,6 +3,12 @@
 # move to script path
 cd `dirname $0`
 
+# set the following env var before running this script
+# * HUBOT_PORT
+# * HUBOT_DOMAIN
+# * HUBOT_IRC_NICK
+# * HUBOT_GITHUB_TOKEN
+
 export PORT=$HUBOT_PORT
 
 export HUBOT_LOG_LEVEL="debug"
@@ -11,9 +17,6 @@ export HUBOT_IRC_ROOMS="#easyrpg"
 export HUBOT_IRC_SERVER="irc.freenode.net"
 export HUBOT_IRC_DEBUG=On
 export HUBOT_IRC_UNFLOOD="true"
-
-# set bot name
-#export HUBOT_IRC_NICK=""
 
 export HUBOT_URL="http://$HUBOT_DOMAIN:$PORT"
 
