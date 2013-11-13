924fa90748bfe8b3b101d2c3276b189c0fd9dd29
take_cheeze <takechi101010@gmail.com>
2013-11-13 09:23:57 +0900

    fix env variable

--- run.sh
+++ run.sh
@@ -4,21 +4,18 @@
 cd `dirname $0`
 
 # set the following env var before running this script
-# * HUBOT_PORT
 # * HUBOT_DOMAIN
-# * HUBOT_IRC_NICK
 # * HUBOT_GITHUB_TOKEN
-
-export PORT=$HUBOT_PORT
 
 export HUBOT_LOG_LEVEL="debug"
 
 export HUBOT_IRC_ROOMS="#easyrpg"
 export HUBOT_IRC_SERVER="irc.freenode.net"
+export HUBOT_IRC_NICK="easyrpg-hubot
 export HUBOT_IRC_DEBUG=On
 export HUBOT_IRC_UNFLOOD="true"
 
-export HUBOT_URL="http://$HUBOT_DOMAIN:$PORT"
+export HUBOT_URL="http://$HUBOT_DOMAIN"
 
 export FEED_CHECK_INTERVAL=60
 
